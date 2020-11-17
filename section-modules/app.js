4b64946b1c6e294d9f3e0e1e1e108edb3b525803
ruddy palvair <r.palvair@gmail.com>
2020-11-17 18:01:08 +0100

    Export variables

--- section-modules/app.js
+++ section-modules/app.js
@@ -3,3 +3,5 @@ const mod1 = require("./module1");
 mod1.sayHello();
 
 mod1.sayHi();
+
+console.log(mod1.hello)
--- section-modules/module1.js
+++ section-modules/module1.js
@@ -1,3 +1,5 @@
 exports.sayHello = () => console.log('Hello')
 
 exports.sayHi = () => console.log('Hi!')
+
+exports.hello = "Hello World"
