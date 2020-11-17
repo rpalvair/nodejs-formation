3f141193eef9d15d22cd846ffbe26fa5dcca1378
ruddy palvair <r.palvair@gmail.com>
2020-11-17 17:48:31 +0100

    feat : Send file content via http if url is '/test' refactor : Extract method

--- section-modules/http.js
+++ section-modules/http.js
@@ -1,17 +1,33 @@
 const http = require("http");
+const fs = require("fs");
 
-http.createServer((req, res) => {
-
-    if(req.url == '/') {
-        res.writeHead(200, {
-            'Content-type': 'text/html'
-        })
-        res.write("<h1>Accueil</h1>\n")
+http
+  .createServer((req, res) => {
+    if (req.url == "/") {
+      res.writeHead(200, {
+        "Content-type": "text/html",
+      });
+      res.write("<h1>Accueil</h1>\n");
+      res.end();
+    } else if (req.url == "/test") {
+      fs.readFile("section-modules/test.txt", "utf-8", (err, data) => {
+        if (err) {
+          send404(res);
+        } else {
+          res.write(data);
+          res.end();
+        }
+      });
     } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html'
-        })
-        res.write("<span style='color:red'>Erreur 404</span>")
+      send404(res);
     }
-    res.end()
-}).listen(8080);
+  })
+  .listen(8080);
+
+function send404(res) {
+  res.writeHead(404, {
+    "Content-type": "text/html",
+  });
+  res.write("<span style='color:red'>Erreur 404</span>");
+  res.end();
+}
