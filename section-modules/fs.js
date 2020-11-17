5076b0c145c96343af7137dedd2824e158552390
ruddy palvair <r.palvair@gmail.com>
2020-11-17 17:05:43 +0100

    Write to a file

--- section-modules/fs.js
+++ section-modules/fs.js
@@ -5,5 +5,8 @@ fs.readFile('section-modules/test.txt', 'utf-8', (err,data) => {
         console.log(err)
     } else {
         console.log(data)
+        fs.writeFile('section-modules/test.txt','Hello World', 'utf-8', (err) => {
+            console.log(err)
+        })
     }
 })
