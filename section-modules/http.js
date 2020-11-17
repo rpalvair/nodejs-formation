91911a4e7d216bbfd5a8b1b8850a66af2dd6eccb
ruddy palvair <r.palvair@gmail.com>
2020-11-17 17:32:26 +0100

    Add header

--- section-modules/http.js
+++ section-modules/http.js
@@ -3,8 +3,14 @@ const http = require("http");
 http.createServer((req, res) => {
 
     if(req.url == '/') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
         res.write("Accueil\n")
     } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        })
         res.write("Erreur 404")
     }
     res.end()
