90c7259e4eadbba440e2c45488d04d6d6c370233
ruddy palvair <r.palvair@gmail.com>
2020-11-17 17:08:24 +0100

    Read file after write

--- section-modules/fs.js
+++ section-modules/fs.js
@@ -1,12 +1,14 @@
-const fs = require('fs')
+const fs = require("fs");
 
-fs.readFile('section-modules/test.txt', 'utf-8', (err,data) => {
-    if(err) {
-        console.log(err)
-    } else {
-        console.log(data)
-        fs.writeFile('section-modules/test.txt','Hello World', 'utf-8', (err) => {
-            console.log(err)
-        })
-    }
-})
+fs.readFile("section-modules/test.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+    fs.writeFile("section-modules/test.txt", "Hello World", "utf-8", (err) => {
+      fs.readFile("section-modules/test.txt", "utf-8", (err, data) => {
+          console.log(data)
+      });
+    });
+  }
+});
