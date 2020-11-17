14500897c62e88e3c5140ee2b8a9594933d53c4b
ruddy palvair <r.palvair@gmail.com>
2020-11-17 18:02:51 +0100

    Exports at the bottom

--- section-modules/module1.js
+++ section-modules/module1.js
@@ -3,3 +3,8 @@ exports.sayHello = () => console.log('Hello')
 exports.sayHi = () => console.log('Hi!')
 
 exports.hello = "Hello World"
+
+
+exports.sayHello = this.sayHello;
+exports.sayHi = this.sayHi;
+exports.hello = this.hello;
