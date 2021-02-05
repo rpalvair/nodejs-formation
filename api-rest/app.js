614f4c77bf5c9254a3829a3c0ddf891fdfff8889
ruddy palvair <r.palvair@gmail.com>
2021-02-05 11:22:35 +0100

    feat : Url with path params

--- api-rest/app.js
+++ api-rest/app.js
@@ -11,5 +11,9 @@ app.get('/api/v1', (req, res) => {
     res.send('API Version 1')
 })
 
+app.get('/api/v1/books/:id', (req,res) => {
+    res.send(req.params)
+})
+
 
 app.listen(8080, () => console.log("Started on port 8080"))
