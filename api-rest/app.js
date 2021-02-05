df0321511abef4b687518f3c118f2fc90ef274e7
ruddy palvair <r.palvair@gmail.com>
2021-02-05 11:18:31 +0100

    feat : Add another page '/api/v1'

--- api-rest/app.js
+++ api-rest/app.js
@@ -7,5 +7,9 @@ app.get('/api',(req, res) => {
     res.send('Root API')
 })
 
+app.get('/api/v1', (req, res) => {
+    res.send('API Version 1')
+})
+
 
 app.listen(8080, () => console.log("Started on port 8080"))
