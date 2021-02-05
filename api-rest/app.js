f654a228f1b9bbe98445d02c2ffbe0b654f095a6
ruddy palvair <r.palvair@gmail.com>
2021-02-05 11:33:03 +0100

    feat  : Create a middleware

--- api-rest/app.js
+++ api-rest/app.js
@@ -1,19 +1,23 @@
-require('babel-register')
+require("babel-register");
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/api',(req, res) => {
-    res.send('Root API')
-})
+app.use((req, res, next) => {
+  console.log("URL : " + req.url);
+  next();
+});
 
-app.get('/api/v1', (req, res) => {
-    res.send('API Version 1')
-})
+app.get("/api", (req, res) => {
+  res.send("Root API");
+});
 
-app.get('/api/v1/books/:id', (req,res) => {
-    res.send(req.params)
-})
+app.get("/api/v1", (req, res) => {
+  res.send("API Version 1");
+});
 
+app.get("/api/v1/books/:id", (req, res) => {
+  res.send(req.params);
+});
 
-app.listen(8080, () => console.log("Started on port 8080"))
+app.listen(8080, () => console.log("Started on port 8080"));
