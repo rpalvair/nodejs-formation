e131e0535275efaf8ccfba50ab45b6d75967a279
ruddy palvair <r.palvair@gmail.com>
2021-02-08 20:30:07 +0100

    feat : Use try-catch with async-await

--- async/run-with-async-wait.js
+++ async/run-with-async-wait.js
@@ -1,9 +1,13 @@
 const { getMemberWithPromise, getArticlesWithPromise } = require("./promise")
 
 exports.runWithAsync = async () => {
-  console.log("runWithAsync")
-  let member = await getMemberWithPromise()
-  console.log("runWithAsync", member)
-  let articles = await getArticlesWithPromise()
-  console.log("runWithAsync", articles)
+  try {
+    console.log("runWithAsync")
+    let member = await getMemberWithPromise()
+    console.log("runWithAsync", member)
+    let articles = await getArticlesWithPromise()
+    console.log("runWithAsync", articles)
+  } catch (e) {
+    console.error(e)
+  }
 }
