a5bc332db55c6f28939e45bb82718f431e555c42
ruddy palvair <r.palvair@gmail.com>
2021-02-08 19:42:07 +0100

    feat : Imbrication of callbacks

--- async/app.js
+++ async/app.js
@@ -1,11 +1,14 @@
 require("babel-register")
 
-const { getMemberWithCallBack } = require("./callback")
+const { getMemberWithCallBack, getArticles } = require("./callback")
 
 console.log("Debut")
 
 getMemberWithCallBack((member) => {
   console.log(member)
+  getArticles(member, (articles) => {
+    console.log(articles)
+  })
 })
 
 console.log("Fin")
--- async/callback.js
+++ async/callback.js
@@ -1,7 +1,14 @@
 getMember = (callback) => {
   setTimeout(() => {
     callback("Member 1")
-  }, 3000)
+  }, 1500)
+}
+
+getArticles = (member, callback) => {
+  setTimeout(() => {
+    callback([1, 2, 3])
+  }, 1500)
 }
 
 exports.getMemberWithCallBack = getMember
+exports.getArticles = getArticles
