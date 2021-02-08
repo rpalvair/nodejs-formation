1838bf57a5f10b79dc1409e37b6a9b6078752eaa
ruddy palvair <r.palvair@gmail.com>
2021-02-08 20:05:06 +0100

    feat : Get member and articles with promises

--- async/app.js
+++ async/app.js
@@ -1,15 +1,14 @@
 require("babel-register")
 
-const { getMemberWithCallBack, getArticles } = require("./callback")
-const { promiseDone, promiseError } = require("./promise")
+const { getMemberWithPromise, getArticlesWithPromise } = require("./promise")
 
 console.log("Debut")
 
-promiseDone()
-  .then((value) => console.log(value))
-  .catch((error) => console.error(error))
-
-promiseError()
+getMemberWithPromise()
+  .then((value) => {
+    console.log(value)
+    return getArticlesWithPromise()
+  })
   .then((value) => console.log(value))
   .catch((error) => console.error(error))
 
--- async/promise.js
+++ async/promise.js
@@ -14,5 +14,25 @@ promiseError = () => {
   })
 }
 
+getMember = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Member 1")
+      //   reject(new Error("arf"))
+    }, 1500)
+  })
+}
+
+getArticles = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([1, 2, 3])
+      //   reject(new Error("arf"))
+    }, 1500)
+  })
+}
+
 exports.promiseDone = promiseDone
 exports.promiseError = promiseError
+exports.getMemberWithPromise = getMember
+exports.getArticlesWithPromise = getArticles
