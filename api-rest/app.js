b4cb3d6a923abb89e7bd218159ea5db5481b2e5e
ruddy palvair <r.palvair@gmail.com>
2021-02-08 10:14:13 +0100

    feat : Get member by id

--- api-rest/app.js
+++ api-rest/app.js
@@ -1,20 +1,27 @@
-require("babel-register")
-const express = require("express")
-const morgan = require('morgan')
-const app = express()
+require("babel-register");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get("/api", (req, res) => {
-  res.send("Root API");
-});
+const members = [
+  {
+    id: 1,
+    name: "John",
+  },
+  {
+    id: 2,
+    name: "Julie",
+  },
+  {
+    id: 3,
+    name: "Jack",
+  },
+];
 
-app.get("/api/v1", (req, res) => {
-  res.send("API Version 1");
-});
-
-app.get("/api/v1/books/:id", (req, res) => {
-  res.send(req.params);
-});
+app.get("/api/v1/members/:id", (req, res) => {
+  res.send(members[req.params.id - 1])
+})
 
 app.listen(8080, () => console.log("Started on port 8080"));
