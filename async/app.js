8d278b9b4a542977442c830c35ffaa62ff1b523b
ruddy palvair <r.palvair@gmail.com>
2021-02-08 20:09:19 +0100

    feat : Parallel execution of promises

--- async/app.js
+++ async/app.js
@@ -4,12 +4,16 @@ const { getMemberWithPromise, getArticlesWithPromise } = require("./promise")
 
 console.log("Debut")
 
-getMemberWithPromise()
-  .then((value) => {
-    console.log(value)
-    return getArticlesWithPromise()
-  })
-  .then((value) => console.log(value))
-  .catch((error) => console.error(error))
+Promise.all([getMemberWithPromise(), getArticlesWithPromise()]).then((value) =>
+  console.log(value)
+)
+
+// getMemberWithPromise()
+//   .then((value) => {
+//     console.log(value)
+//     return getArticlesWithPromise()
+//   })
+//   .then((value) => console.log(value))
+//   .catch((error) => console.error(error))
 
 console.log("Fin")
