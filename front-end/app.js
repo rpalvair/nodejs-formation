e20fdbd8c3831457bcb0c76e663c3fce8fbbc2df
ruddy palvair <r.palvair@gmail.com>
2021-02-10 18:44:26 +0100

    feat : Use twig as view engine

--- front-end/app.js
+++ front-end/app.js
@@ -2,16 +2,19 @@ require("babel-register")
 const express = require("express")
 const config = require("./config.json")
 const morgan = require("morgan")
-
+const Twig = require("twig")
 
 const app = express()
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.set("view engine", "twig")
 
-app.get("/",(req,res) => {
-    res.send("Okay")
+app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/views/index.html")
+  res.render('index.twig', {
+    message : "I'm a FullStack Developer"
+  });
 })
-
 
 app.listen(config.port, () => console.log(`Started on port ${config.port}`))
