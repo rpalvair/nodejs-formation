88298f51a8a788f94b13b89f1e4b8f4bf69da0e1
ruddy palvair <r.palvair@gmail.com>
2021-02-10 19:17:59 +0100

    feat : Http request with axios and error page

--- front-end/app.js
+++ front-end/app.js
@@ -2,6 +2,7 @@ require("babel-register")
 const express = require("express")
 const config = require("./config.json")
 const morgan = require("morgan")
+const axios = require("axios")
 
 const app = express()
 app.use(morgan("dev"))
@@ -17,9 +18,21 @@ router.route("/").get((req, res) => {
 })
 
 router.route("/members").get((req, res) => {
-  res.send("WIP")
+  axios.get("http://localhost:8080/api/v1/member")
+  .then((response) => {
+    
+  }).catch((err) => {
+    renderError(res, err.message)
+  })
 })
 
 app.use(config.basePath, router)
 
 app.listen(config.port, () => console.log(`Started on port ${config.port}`))
+
+
+function renderError(res, message) {
+  res.render("error.twig", {
+    errorMessage: message,
+  })
+}
