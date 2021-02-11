f635ec3f2a5f1fcaf9e7237fcfc0c6e7e033eef6
ruddy palvair <r.palvair@gmail.com>
2021-02-11 10:24:44 +0100

    feat : Page for one member

--- front-end/app.js
+++ front-end/app.js
@@ -21,20 +21,19 @@ router.route("/").get((req, res) => {
 })
 
 router.route("/members").get((req, res) => {
-  fetch
-    .get("/members")
-    .then((response) => {
-      if (response.data.status == "success") {
-        res.render("members.twig", {
-          members: response.data.result,
-        })
-      } else {
-        renderError(res, response.data.message)
-      }
+  apiCall("/members", res, (value) => {
+    res.render("members.twig", {
+      members: value,
     })
-    .catch((err) => {
-      renderError(res, err.message)
+  })
+})
+
+router.route("/members/:id").get((req, res) => {
+  apiCall("/members/" + req.params.id, res, (value) => {
+    res.render("member.twig", {
+      member: value,
     })
+  })
 })
 
 app.use(config.basePath, router)
@@ -45,4 +44,19 @@ function renderError(res, message) {
   res.render("error.twig", {
     errorMessage: message,
   })
+}
+
+function apiCall(url, res, successCallback) {
+  fetch
+    .get(url)
+    .then((response) => {
+      if (response.data.status == "success") {
+        successCallback(response.data.result)
+      } else {
+        renderError(res, response.data.message)
+      }
+    })
+    .catch((err) => {
+      renderError(res, err.message)
+    })
 }
